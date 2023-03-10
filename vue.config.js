01c24db7f12dfbb0de112e7af0cd1b8feb53eee2
Валёк <nelil1990@mail.ru>
2023-03-10 17:58:02 +0700

    app created

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    proxy: {
+      '^/users': {
+        target: 'http://127.0.0.1:8000/',
+        ws: true,
+        changeOrigin: true
+      },
+    }
+  },
   transpileDependencies: true
 })
